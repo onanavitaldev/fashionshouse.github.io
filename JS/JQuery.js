1042b4e5ce1cb498e9e6b3897d3a0ba23119acc4
NobiQ <102168229+onanavitaldev@users.noreply.github.com>
2022-11-12 01:20:41 +0100

    Add files via upload

---
+++ JS/JQuery.js
@@ -0,0 +1,18 @@
+/* __ScrollReveal/Animation__ */
+window.sr = ScrollReveal();
+
+sr.reveal('#image1', {
+    duration: 3000,
+    origin: 'right',
+    distance: '100px',
+});
+sr.reveal('#image2', {
+    duration: 4000,
+    origin: 'left',
+    distance: '100px',
+});
+sr.reveal('#picture', {
+    duration: 2000,
+    origin: 'top',
+    distance: '100px',
+});
